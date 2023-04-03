799ca0386d6366bd07e23e936f07ff631a05f496
waqas01~ <waqasch2806@gmail.com>
2023-04-03 10:49:05 -0700

    style updates

--- src/app/layout/main-layout/cover-image/cover-image.component.ts
+++ src/app/layout/main-layout/cover-image/cover-image.component.ts
@@ -6,5 +6,48 @@ import { Component } from '@angular/core';
   styleUrls: ['./cover-image.component.scss']
 })
 export class CoverImageComponent {
+  navLinks:any=[
+    {
+      text:'Writers',
+      route:'' //we can add route links here
+    },
+    {
+      text:'Samples',
+    },
+    {
+      text:'Pricing',
+    },
+    {
+      text:'FAQs',
+    },
+    {
+      text:'Why Us',
+    },
+    {
+      text:'Reviews'
+    }
+  ]
+  items=[
+    {
+      heading:'250+',
+      text:'Academic Writing Experts',
+      icon:'assets/images/pencil.svg'
+    },
+    {
+      heading:'50%',
+      text:'Money Back Guarantee',
+      icon:'assets/images/diamond.svg'
+    },
+    {
+      heading:'100%',
+      text:'Plagiarism Free Content',
+      icon:'assets/images/diamond.svg'
+    },
+    {
+      heading:'50%',
+      text:'Prepay Upfront - Half After We Deliver',
+      icon:'assets/images/Star 1.svg'
+    },
 
+  ]
 }
