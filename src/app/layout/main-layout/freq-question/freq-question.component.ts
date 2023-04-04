2550b4a9e9eb35ce8bdd509990acd181b452d05a
waqas01~ <waqasch2806@gmail.com>
2023-04-04 14:29:35 -0700

    answer to the question added

--- src/app/layout/main-layout/freq-question/freq-question.component.ts
+++ src/app/layout/main-layout/freq-question/freq-question.component.ts
@@ -6,15 +6,31 @@ import { Component } from '@angular/core';
   styleUrls: ['./freq-question.component.scss']
 })
 export class FreqQuestionComponent {
+  visible:any
   items=[
     {
+      id:1,
       text:"I need someone to write my essay for me, who can help me?",
+      active:false
     },
     {
+      id:2,
       text:"What you should not write in a college essay?",
+      active:false
     },
     {
+      id:3,
       text:"Can you write my essay for free?",
+      active:false
+
     }
   ]
+  questionClick(item:any){
+    this.visible = item; item.active = !item.active
+    this.items.forEach((itemx:any)=>
+    {
+      if(itemx.id!==item.id)
+       itemx.active=!item.active
+    })
+  }
 }
